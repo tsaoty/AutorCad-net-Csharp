393efa04df3793cb0ca3badd95b605d7ff69a710
DESKTOP-0N8UFAQ\tsaoty <tsaoty@gmail.com>
2024-06-16 16:07:01 +0800

    利用自建 main，Debug .dll

---
+++ 90_ClassDebug/展示工具/Program.cs
@@ -0,0 +1,35 @@
+﻿using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"tty msg>Input: {input}");
+            Console.WriteLine("ty msg> Begins with uppercase? " +
+                $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+        return;
+
+        // Declare a ResetConsole local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("tty msg> Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"tty msg> {Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press<Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
