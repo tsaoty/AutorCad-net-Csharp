fade05e80800dc21dd7b9c67988a6dddfa5c25d1
DESKTOP-0N8UFAQ\tsaoty <tsaoty@gmail.com>
2024-06-23 09:22:25 +0800

    InitAndOpt 加入 change 及 try...catch

--- Chap01_net8/Hello_tty/obj/x64/Debug/net8.0/Hello_tty.AssemblyInfo.cs
+++ Chap01_net8/Hello_tty/obj/x64/Debug/net8.0/Hello_tty.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello_tty")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bec367ec276edab26e7d8a00f97bad5d06a8e9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52744835865ed9312b79c7a6ef5ecfcd6b5ed697")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello_tty")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello_tty")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
